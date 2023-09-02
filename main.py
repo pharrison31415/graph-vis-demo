779b16c9ecf641e68b58f3511c48ac17813723a8
Paul Harrison <a02382371@aggies.usu.edu>
2023-09-02 16:59:12 -0600

    add gravity to ax, ay

--- main.py
+++ main.py
@@ -38,15 +38,27 @@ def main():
             if event.type == pygame.QUIT or (event.type == pygame.KEYUP and event.key == pygame.K_ESCAPE):
                 sys.exit()
 
+        # update vertex accelerations
+        for v1 in vertices:
+            v1.ax = 0
+            v1.ay = 0
+            for v2 in vertices:
+                if v1 == v2:
+                    continue
+
+                grav = v1.gravity(v2)
+                v1.ax += grav[0]
+                v2.ay += grav[1]
+
         # update vertex positions and draw
         for vertex in vertices:
             vertex.update_pos()
-            pygame.draw.circle(surface, (255, 255, 0),
+            pygame.draw.circle(surface, (255, 255, 255),
                                (vertex.x, vertex.y), vertex.radius)
 
         # draw edges
         for edge in edges:
-            pygame.draw.line(surface, (255, 255, 0),
+            pygame.draw.line(surface, (255, 255, 255),
                              (edge.v1.x, edge.v1.y), (edge.v2.x, edge.v2.y))
 
         pygame.display.update()
