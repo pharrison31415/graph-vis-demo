257a1ae526fea6413cf6894b8bb7df03804aa554
Paul Harrison <a02382371@aggies.usu.edu>
2023-09-02 16:57:53 -0600

    vertex gravity

--- Vertex.py
+++ Vertex.py
@@ -1,9 +1,11 @@
 from random import uniform
+from math import sqrt
 
 
 class Vertex:
     WIDTH, HEIGHT = 1200, 600
     TEMP = 0
+    GRAV = -1
 
     def __init__(self):
         self.x = uniform(0, Vertex.WIDTH)
@@ -13,11 +15,26 @@ class Vertex:
         self.ax = 0
         self.ay = 0
 
-        self.radius = 4
+        self.m = 1
+
+        self.radius = 6
 
         self.edges = set()
 
-    def update_pos(self, dt=0.01):
+    def gravity(self, vertex):
+        delta_x = self.x - vertex.x
+        delta_y = self.y - vertex.y
+
+        # divibe by zero
+        if (delta_x, delta_y) == (0, 0):
+            return (0, 0)
+
+        dist = sqrt(delta_x**2 + delta_y**2)
+        magnitude = Vertex.GRAV * self.m * vertex.m / dist**2
+
+        return [magnitude * delta_x/dist, -magnitude * delta_y/dist]
+
+    def update_pos(self, dt=1):
         self.dx += self.ax*dt
         self.dy += self.ay*dt
 
@@ -27,12 +44,16 @@ class Vertex:
         if self.x < self.radius:
             self.x = self.radius
             self.dx = 0
+            self.ax = 0
         if self.x > Vertex.WIDTH-self.radius:
             self.x = Vertex.WIDTH-self.radius
             self.dx = 0
+            self.ax = 0
         if self.y < self.radius:
             self.y = self.radius
             self.dy = 0
+            self.ay = 0
         if self.y > Vertex.HEIGHT-self.radius:
             self.y = Vertex.HEIGHT-self.radius
             self.dy = 0
+            self.ay = 0
